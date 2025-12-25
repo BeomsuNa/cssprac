e54e17accec06d7c18ed97d61707abde6c08a81e
나범수 <123361606+BeomsuNa@users.noreply.github.com>
2025-12-25 21:19:15 +0900

    버튼 레이아웃 css

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // app/layout.tsx
+import { Button } from '@/components/ui/button';
 import './globals.css';
 import Link from 'next/link';
 
@@ -13,10 +14,10 @@ export default function RootLayout({
         <header>
             <h2>해당 링크들을 클릭하세요</h2>
           <nav className='grid grid-rows-2 grid-cols-2 gap-4 '>
-            <Link href="/">Home</Link>
-            <Link href="/pinpage">Pin Page</Link>
-            <Link href="/scrollpage">Scroll Page</Link>
-            <Link href="/csspage">CSS Page</Link>
+            <Button><Link href="/">Home</Link></Button>
+            <Button><Link href="/pinpage">Pin Page</Link></Button>
+            <Button><Link href="/scrollpage">Scroll Page</Link></Button>
+            <Button><Link href="/csspage">CSS Page</Link></Button>
           </nav>
         </header>
         <main>{children}</main>
